a452b94d25f2a3a45c810f9c99c4af7b530a9078
Ctucker9233 <court.m.tucker@gmail.com>
2019-10-06 14:33:22 -0700

    Adding basic starting files to the repository and populated my questions js

---
+++ JS/questions.js
@@ -0,0 +1,22 @@
+var questionsObj = [
+    {
+        title: "Comonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed within _________.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store _________.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above";
+    },
+    {
+        title: "String values must be enclosed within _________ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        anwser: "quotes";
+    }
+]
